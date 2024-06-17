5098c1d11449b480ed1bdc39b5b01b9613eeae53
alexcarnes <alexcarnes@outlook.com>
2024-06-17 13:05:49 -0500

    update

--- Q-1/Q1.cpp
+++ Q-1/Q1.cpp
@@ -7,20 +7,22 @@ alex carnes 6/6
 
 using namespace std;
 
-
-
-
-
 int main() {
 
-    int a,b;
+    int a,b,r,s;
 
-    cout << "enter starting number:  "; cin >> a;
-    cout << "enter range:  "; cin >> b;
+    cout << "enter starting number:  "; cin >> s;
+    cout << "enter range:  "; cin >> r;
+
+    if (s < r) {b = r; a = s;}
+    else (a = r); (b = s);
+
+    for ( int i = a; i <= b; i++) {
+
+        if (i == 4 || i == 13) {}
+        else cout << i << endl;
+    }
 
 
-
-
-
-	return 0;
+    return 0;
 }
