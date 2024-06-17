f9aaf3c9d6d39d9525e64d93468d0c0ec5778afd
alexcarnes <alexcarnes@outlook.com>
2024-06-17 11:27:16 -0500

    update

--- Q1/Q1.cpp
+++ Q1/Q1.cpp
@@ -7,7 +7,19 @@ alex carnes 6/6
 
 using namespace std;
 
+
+
+
+
 int main() {
+
+    int a,b;
+
+    cout << "enter starting number:  "; cin >> a;
+    cout << "enter range:  "; cin >> b;
+
+
+
 
 
 	return 0;
