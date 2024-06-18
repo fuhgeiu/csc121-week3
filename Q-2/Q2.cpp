d71123fe1e516a42927a1311dfacc4cef50377c1
alexcarnes <alexcarnes@outlook.com>
2024-06-18 11:29:10 -0500

    update

--- Q-2/Q2.cpp
+++ Q-2/Q2.cpp
@@ -9,7 +9,18 @@ using namespace std;
 
 int main() {
 
+    int my_array[5];
+
+    cout << "enter 5 score\n\n";
+
+    for (int i = 0; i < 5; i++) {
+
+        cin >> my_array[i];
 
 
-	return 0;
+    }
+
+    for (int i = 0; i < 5; i++) cout << my_array[i];
+
+    return 0;
 }
