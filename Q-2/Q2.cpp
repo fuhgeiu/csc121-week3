feb6d5e274bac56e9a02e047c30cf00c512f967f
alexcarnes <alexcarnes@outlook.com>
2024-06-18 18:26:21 -0500

    update

--- Q-2/Q2.cpp
+++ Q-2/Q2.cpp
@@ -7,10 +7,25 @@ alex carnes 6/6
 
 using namespace std;
 
+void get_S (int [],double&);
+void dis_S (int [],double T);
+
+
 int main() {
 
     int my_array[5];
     double T;
+
+//get scores and calculate
+    get_S (my_array,T);
+
+//  display both
+    dis_S(my_array,T);
+
+    return 0;
+}
+
+void get_S (int my_array[],double &T) {
 
     cout << "enter 5 score\n\n";
 
@@ -19,14 +34,13 @@ int main() {
 //        calculate total
         T = T + my_array[i];
     }
+}
 
-//  calculate average
-//  display both
+void dis_S (int my_array[], double T) {
+
     cout << "the total is: " << T << endl;
     cout << "the average is: " << T/5 << endl;
 
     cout << "entered 5 scores are:\n";
-        for (int i = 0; i < 5; i++) cout << my_array[i] << endl;
-
-    return 0;
+    for (int i = 0; i < 5; i++) cout << my_array[i] << endl;
 }
