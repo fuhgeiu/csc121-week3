a628e61c62e4b205b1182f4c4db15fd222de1626
alexcarnes <alexcarnes@outlook.com>
2024-06-18 12:08:18 -0500

    update

--- Q-2/Q2.cpp
+++ Q-2/Q2.cpp
@@ -10,17 +10,27 @@ using namespace std;
 int main() {
 
     int my_array[5];
+    double T;
 
     cout << "enter 5 score\n\n";
 
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i <= 4; i++) {
 
         cin >> my_array[i];
-
-
+//        calculate total
     }
 
-    for (int i = 0; i < 5; i++) cout << my_array[i];
+    for (int i = 0; i < 5; i++)
+        T =+ my_array[i];
+
+
+//  calculate average
+//  display both
+    cout << "the total is: " << T << endl;
+    cout << "the average is: " << T/5 << endl;
+
+    cout << "entered 5 scores are:\n";
+        for (int i = 0; i < 5; i++) cout << my_array[i] << endl;
 
     return 0;
 }
